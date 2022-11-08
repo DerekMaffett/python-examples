6eb40988b0d47514800e200be402fe6d60d3b4f8
Derek Maffett <derekgmaffett@gmail.com>
2022-11-08 22:24:53 +0100

    add more docs

--- src/collection.py
+++ src/collection.py
@@ -1,3 +1,7 @@
+# NOTE: filter is an actual function in the core library so we name this _filter to differentiate it. It 
+# does basically the same thing.
+
+# This takes a list of items and a function f(x) returning a boolean, and returns the items that satistfy the function
 def _filter(filter_fn, items):
     returned_items = []
 
@@ -8,6 +12,8 @@ def _filter(filter_fn, items):
     return returned_items
 
 
+# This is almost the same as filter, but returns the index of the items instead. The implementation is
+# very similar overall, and now we can ignore the list-manipulation logic for the rest of our problems. 
 def filter_for_indices(filter_fn, items):
     returned_items = []
 
@@ -16,3 +22,19 @@ def filter_for_indices(filter_fn, items):
             returned_items.append(index)
 
     return returned_items
+
+# While the above particular function is weird, most common operations on lists are covered in the core library or
+# other common utility libraries.
+
+# Examples:
+#     - map - apply a transformation to each item in a list
+#     - filter - create a new list of items matching a criteria
+#     - reduce - iterate through a list to crunch them down to a single value or otherwise smaller data point
+#     - find - return the first item in a list that matches a criteria, if any match
+#     - any - do any items in this list fit a criteria?
+#     - all - do ALL of the items in this list fit a criteria?
+#     - zip - combine pairwise the items in two lists (like a zipper)
+
+# These can all be done with for-loops and Python does appear to make greater use of them than many other languages, 
+# but understanding especially the first four example operations (map/filter/reduce/find) can make it MUCH easier 
+# to focus on the actual logic of the unique problem you're approaching. 
--- src/collection_test.py
+++ src/collection_test.py
@@ -8,3 +8,16 @@ def test_filter():
 
 def test_filter_for_indices():
     assert filter_for_indices(is_even, [100, 12, 33, 97]) == [0, 1]
+
+
+# NOTE: UNNECESSARY BUT INTERESTING INFORMATION AHEAD!
+# The following test is just to show that the core library version of `filter` is very similar
+# to what we designed here. The extra "list" call is needed simply because filter returns 
+# an iterator rather than an actual array. While I'm not an expert on the why behind this, it basically
+# means that the data needs to be traversed in order to make it "real" - until then it's more of a system
+# that can *create* an array. 
+
+# One reason for this type of construction is lazy evaluation - an array with a billion items might overload 
+# memory, but an Iterator could have infinite items without issue as long as a program only asks for ten of them.
+def test_core_library_filter():
+    assert list(filter(is_even, [1, 2, 3, 4, 5])) == [2, 4]
